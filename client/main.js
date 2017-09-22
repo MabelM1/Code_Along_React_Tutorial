6fc5708cccc013ec0a1bfd275176093188d19530
Mabel <macmota@hotmail.com>
2017-09-21 23:52:31 -0400

    updating score

--- client/main.js
+++ client/main.js
@@ -7,7 +7,18 @@ import {Tracker} from 'meteor/tracker';
 
 const renderPlayers = (playersList) => {
    return playersList.map((player) => {
-    return <p key={player._id}>{player.name} has {player.score} point(s).</p>
+    return (
+    <p key={player._id}>
+      {player.name} has {player.score} point(s).
+      <button onClick={() => {
+       Players.update({_id: player._id}, {$inc: {score: 1}});
+      }}>+1</button>
+      <button onClick={() => {
+       Players.update({_id: player._id}, {$inc: {score: -1}});
+      }}>-1</button>
+      <button onClick={() => Players.remove({_id: player._id})}>X</button>
+    </p>
+    );
   });
 };
 
