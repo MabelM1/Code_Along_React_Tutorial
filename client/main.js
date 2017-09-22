c8e55242260f6c547da119de974fc7bbb6391fc3
Mabel <macmota@hotmail.com>
2017-09-22 16:56:29 -0400

    creating the app component

--- client/main.js
+++ client/main.js
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom';
 import { Meteor } from 'meteor/meteor';
 import {Players} from './../imports/api/players';
 import {Tracker} from 'meteor/tracker';
-import TitleBar from './../imports/ui/TitleBar';
-import AddPlayer from './../imports/ui/AddPlayer';
-import PlayerList from './../imports/ui/PlayerList';
+import App from './../imports/ui/App';
 
 
 
@@ -14,14 +12,6 @@ Meteor.startup(() => {
   Tracker.autorun(() => {
     let players = Players.find().fetch();
     let title = 'Score keep';
-    let jsx = (
-      <div>
-        <TitleBar title={title} subtitle="Created by Me"/>     
-          <PlayerList players={players}/>
-          <AddPlayer/>         
-      </div>
-    );
-    ReactDOM.render(jsx, document.getElementById('app'));
+    ReactDOM.render(<App title={title} players={players}/>, document.getElementById('app'));
   });
-
 });
--- imports/ui/PlayerList.js
+++ imports/ui/PlayerList.js
@@ -1,14 +1,18 @@
 import React from 'react';
-
 import Player from './Player';
 import PropTypes from 'prop-types';
 
 
 export default class PlayerList extends React.Component {
   renderPlayers() {
-  	return this.props.players.map((player) => {
-      return <Player key={player._id} player={player}/>;
-    });
+  	if (this.props.players.length === 0) {
+      return <p>Add your first player to get starter!</p>
+  	} else {
+  	  return this.props.players.map((player) => {
+        return <Player key={player._id} player={player}/>;
+      });	
+  	}
+
   }
   render() {
   	return (
