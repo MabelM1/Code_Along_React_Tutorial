3b61b596d15774f1bdf12d29b3d9a29b4385f96f
Mabel <macmota@hotmail.com>
2017-09-22 15:26:24 -0400

    Using components

---
+++ imports/ui/PlayerList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import Player from './Player';
+import PropTypes from 'prop-types';
+
+
+export default class PlayerList extends React.Component {
+  renderPlayers() {
+  	return this.props.players.map((player) => {
+      return <Player key={player._id} player={player}/>;
+    });
+  }
+  render() {
+  	return (
+      <div>
+       {this.renderPlayers()}
+      </div>
+  	);
+  }
+};
+
+PlayerList.propTyper = {
+  players: PropTypes.array.isrequired	
+}
--- server/main.js
+++ server/main.js
@@ -2,5 +2,11 @@ import {Meteor} from 'meteor/meteor';
 import {Players} from './../imports/api/players';
 
 Meteor.startup(() => {
+  class Person {
+    constructor(name) {
+      this.name = name;
+    }    
+  }
 
+  let me = new Person();
 });
